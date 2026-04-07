dc0300d5f750974b30c235483b40c5b357547322
nsapscience <sappecknoel@gmail.com>
2026-04-07 09:06:12 +0200

    Refactor functions for ESP32 communication and deviation
    Berechnung der Abweichung

--- main.py
+++ main.py
@@ -24,15 +24,17 @@ try:
 except serial.SerialException:
     print("Fehler: Verbindung zu ESP32 konnte nicht hergestellt werden")
 
-#Erstellt Zahl
-def zufall():
-    while True: 
-        global r
-        r = random.randint(1, 4)
-        time.sleep(3)
+#Berechnung der Abweichung
+def deviation():
+    global deviation #Für andere Funktionen veröffentlicht
+    deviation = (calculation_color(ideal_color, color)*0,X + #Berechnung Farbe
+                 calculation_scratch(ideal_scratch, scratch)*0,X #Berechnung Kratzer
+                ... #weitere Berechnungen
+                )
+    def calculation():
 
-#Gibt Zahl an ESP32 weiter
-def weitergabe():
+#Rückmeldung an ESP32 was gerade passiert und was mit dem Produkt ist (gut, schlecht, unsicher, überprüft)
+def transfer():
     while True:
         if ser:
             ser.write(str(r).encode())
@@ -64,8 +66,8 @@ def window():
 
 #Verbindet alle benötigte Funktionen miteinander
 def main():
-    producer = Thread(target=weitergabe)
-    consumer = Thread(target=zahl)
+    producer = Thread(target=transfer)
+    consumer = Thread(target=deviation)
 
     producer.start()
     consumer.start()
