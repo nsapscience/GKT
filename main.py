66fb27bd7e644cec3f4e2315a841bec7df0e111d
nsapscience <sappecknoel@gmail.com>
2026-04-07 09:25:09 +0200

    Update main.py
    def status, output_deviation überarbeitet

--- main.py
+++ main.py
@@ -24,21 +24,33 @@ try:
 except serial.SerialException:
     print("Fehler: Verbindung zu ESP32 konnte nicht hergestellt werden")
 
+
+
+
 #Berechnung der Abweichung
-def deviation():
+def output_deviation():
+    #Berechnung der Werte
+    def calculation(ideal_value, value):
+        return abs(value - ideal_value) / ideal_value * 100
+        
     global deviation #Für andere Funktionen veröffentlicht
-    deviation = (calculation_color(ideal_color, color)*0,X + #Berechnung Farbe
-                 calculation_scratch(ideal_scratch, scratch)*0,X #Berechnung Kratzer
-                ... #weitere Berechnungen
+    #Berechnung der Gesamtabweichung
+    deviation = (calculation(ideal_color, color)*0,X + #Berechnung Farbe
+                 calculation(ideal_scratch, scratch)*0,X #Berechnung Kratzer
+                # + weitere Berechnungen
                 )
-    def calculation():
+    #Muss noch 0,X anpassen!!!
+    #Soll die Werte zur Berechnugn von der Kamera und KI bekommen
+    
+#Gibt an welchen Status das Programm gerade hat und was gut/schlecht ist für Produkte
+def status():
+    print("Hello World!")
 
 #Rückmeldung an ESP32 was gerade passiert und was mit dem Produkt ist (gut, schlecht, unsicher, überprüft)
 def transfer():
     while True:
         if ser:
             ser.write(str(r).encode())
-            print("Gesendete Zahl: ", r)
         time.sleep(3)
 
 #Erstellt das Fenster
@@ -57,17 +69,17 @@ def window():
     
     def update_text():
         error_typ.config(error_typ=str(r))
-        procent.config(procent=str(p) + "%")
+        procent.config(procent=f"Abweichung: {deviation:.1f}%")
+        #Aktualisiert das Fenster alle 200ms
         window.after(200, update_text)
 
     update_text()
     window.mainloop()
 
-
 #Verbindet alle benötigte Funktionen miteinander
 def main():
     producer = Thread(target=transfer)
-    consumer = Thread(target=deviation)
+    consumer = Thread(target=output_deviation)
 
     producer.start()
     consumer.start()
