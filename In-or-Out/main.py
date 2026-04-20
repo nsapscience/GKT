d215802959c22169982a6bcd5d3a5bbc9ef9da4e
nsapscience <sappecknoel@gmail.com>
2026-04-20 12:32:31 +0200

    anfang

--- In-or-Out/main.py
+++ In-or-Out/main.py
@@ -1,1 +1,15 @@
+#Import aller benötigte Komponenten
+from ultralytics import YOLO
+import time
+from Thread import threading
 
+#Eingang von der Maschine bekommen
+
+
+def analyse()
+
+
+
+def main
+  t.analyse = Thread(target=analyse)
+  t.analyse
